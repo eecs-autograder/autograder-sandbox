119f315b6f93ab6ce2381e6d9803815669273362
James P <jameslp@umich.edu>
2017-01-05 19:44:52 -0500

    Updated setup.py license, description, and url

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from setuptools import setup
 
 setup(name='autograder-sandbox',
       version='1.0.0',
-      description='Docker wrapper for securely running untrusted code',
+      description=('Python library for running untrusted '
+                   'code in Docker containers'),
       author='James Perretta',
       author_email='jameslp@umich.edu',
       packages=["autograder_sandbox"],
+      url='https://github.com/eecs280staff/autograder-sandbox',
+      license='GNU Lesser General Public License v3',
       install_requires=['redis>=2.10.5'])
