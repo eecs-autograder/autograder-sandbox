4e0cc0de89af5fe50c80a5490ddebd5938fabc2b
James Perretta <jameslp@umich.edu>
2023-08-25 08:51:30 -0400

    Updated exports.

--- autograder_sandbox/__init__.py
+++ autograder_sandbox/__init__.py
@@ -1,7 +1,10 @@
 from .autograder_sandbox import (
     AutograderSandbox as AutograderSandbox,
     CompletedCommand as CompletedCommand,
+    SandboxError as SandboxError,
     SandboxCommandError as SandboxCommandError,
+    CriticalSandboxError as CriticalSandboxError,
+    SandboxNotDestroyed as SandboxNotDestroyed,
     SANDBOX_USERNAME as SANDBOX_USERNAME,
     SANDBOX_HOME_DIR_NAME as SANDBOX_HOME_DIR_NAME,
     SANDBOX_DOCKER_IMAGE as SANDBOX_DOCKER_IMAGE
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='autograder-sandbox',
-      version='6.0.0a1',
+      version='6.0.0a3',
       description=('Python library for running untrusted '
                    'code in Docker containers'),
       author='James Perretta',
